5f830e7aeb7bdae342e240fb3b21ed6f6fcac29c
Felix Everett <felixreverett@gmail.com>
2024-08-14 21:14:44 +0100

    Changelog ??/??/24a "??" [stable]
    Added TileEntity.cs, TileEntitySaveData.cs
    - Added Tile entities, a type of entity with fixed "integer" coordinates

---
+++ Entities/TileEntitySaveData.cs
@@ -0,0 +1,15 @@
+﻿namespace FeloxGame.Entities
+{
+    public class TileEntitySaveData : EntitySaveData
+    {
+        public float[] Position { get; set; }
+        public float[] Size { get; set; }
+        public float[] DrawPositionOffset { get; set; }
+
+        public TileEntitySaveData(float[] position, float[] size, float[] drawPositionOffset)
+            : base(position, size)
+        {
+            DrawPositionOffset = drawPositionOffset;
+        }
+    }
+}
