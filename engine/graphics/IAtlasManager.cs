ff3ff80374c6b9578521e1246d229ffe4db2c121
Felix Everett <felixreverett@gmail.com>
2025-08-08 14:23:50 +0100

    ✨ New IAtlasManager interface

---
+++ engine/graphics/IAtlasManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Isola.Drawing
+{
+    public interface IAtlasManager
+    {
+        string AtlasFileName { get; }
+
+        //int AtlasSize { get; }
+        //int AtlasWidth { get; }
+        //int AtlasHeight { get; }
+    }
+}
