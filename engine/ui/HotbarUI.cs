09bd1bf8454d3b98371458de8b4a8a50b894deed
Felix Everett <felixreverett@gmail.com>
2026-02-14 19:45:47 +0000

    ✨ Added countTextHelper UI element to handle all Hotbar text rendering; 🧹 Refactored file to use K&R styling.

--- engine/ui/HotbarUI.cs
+++ engine/ui/HotbarUI.cs
@@ -6,11 +6,11 @@ using OpenTK.Mathematics;
 using Isola.Utilities;
 using Isola.World;
 using Isola.Entities;
+using Isola.game.GUI;
+using Isola.engine.graphics.text;
 
-namespace Isola.ui
-{
-    public class HotbarUI : UI
-    {
+namespace Isola.ui {
+    public class HotbarUI : UI {
         // fields
         private int _rows;
         private int _cols;
@@ -22,7 +22,9 @@ namespace Isola.ui
         private PlayerEntity OwnerPlayer;
         protected PrecisionTextureAtlasManager AtlasManager;
 
-        public bool ToggleScrolling { get; set; } = true; //todo: improve this name
+        public bool AllowScrolling { get; set; } = true;
+
+        private TextUI _countTextHelper;
 
         /// <summary>
         /// Creates a HotbarUI class to display an associated inventory
@@ -41,13 +43,10 @@ namespace Isola.ui
         /// <param name="edgePadding">The amount of padding between the edge of the itemslots and the edge of the UI element</param>
         /// <param name="itemSlotPadding">The amount of padding between item slots</param>
         /// <param name="inventory">The associated inventory of the UI element</param>
-        public HotbarUI
-        (
+        public HotbarUI (
             float width, float height, eAnchor anchor, float scale, bool isDrawable, bool toggleDraw, bool isClickable,
             int rows, int cols, float itemSlotHeight, float itemSlotWidth, float edgePadding, float itemSlotPadding, Inventory inventory, PlayerEntity ownerPlayer, string atlasName
-        )
-            : base(width, height, anchor, scale, isDrawable, toggleDraw, isClickable)
-        {
+        ) : base(width, height, anchor, scale, isDrawable, toggleDraw, isClickable) {
             _rows = rows;
             _cols = cols;
             _itemSlotHeight = itemSlotHeight;
@@ -60,26 +59,67 @@ namespace Isola.ui
             BatchRenderer = AssetLibrary.BatchRendererList[atlasName];
             SetTextureCoords(0, 118, 188, 26);
             GenerateChildren();
+            _countTextHelper = new TextUI(16f, 16f, eAnchor.BottomRight, 1.0f, true, true, false, "0", 12, true, "Font Atlas");
         }
 
-        public void GenerateChildren()
-        {
+        public override void Draw() {
+            base.Draw();
+            int totalSlots = _rows * _cols;
+            for (int i = 0; i < totalSlots; i++) {
+                ItemStack item = Inventory.ItemStackList[i];
+
+                if (!item.Equals(default(ItemStack)) && item.Amount > 1) {
+                    if (Children.TryGetValue(i.ToString(), out UI slotUI)) {
+                        string amountText = item.Amount.ToString();
+                        _countTextHelper.Text = amountText;
+
+                        float textPixelWidth = 0f;
+                        float fontScale = (float)_countTextHelper.FontSize / _countTextHelper.FontAtlasManager.LineHeight;
+
+                        foreach (char c in amountText) {
+                            if (_countTextHelper.FontAtlasManager.Characters.TryGetValue(c, out CharData charData)) {
+                                textPixelWidth += charData.XAdvance * fontScale;
+                            }
+                        }
+                        float ndcPixelW = (1f / 640f) * 2f;
+                        float ndcPixelH = (1f / 360f) * 2f;
+
+                        float slotRight = slotUI.NDCs.MaxX;
+                        float slotBottom = slotUI.NDCs.MinY;
+
+                        float padX = 2f * ndcPixelW;
+                        float padY = 2f * ndcPixelH;
+
+                        float textWidthNDC = textPixelWidth * ndcPixelW;
+                        float startX = slotRight - padX - textWidthNDC;
+
+                        float verticalOffset = 9f * ndcPixelH;
+
+                        float startY = slotBottom + padY - verticalOffset;
+
+                        float boxWidthNDC = 16f * ndcPixelW;
+                        float boxHeightNDC = 16f * ndcPixelH;
+
+                        _countTextHelper.NDCs = new NDC( startX, startY, startX + boxWidthNDC, startY + boxHeightNDC);
+
+                        _countTextHelper.Draw();
+                    }
+                }
+            }
+        }
+
+        public void GenerateChildren() {
             int slotIndex = 0;
 
-            for (int row = 0; row < _rows; row++)
-            {
-                for (int col = 0; col < _cols; col++)
-                {
+            for (int row = 0; row < _rows; row++) {
+                for (int col = 0; col < _cols; col++) {
                     RPC childPosition = new();
 
                     childPosition.MinX = _edgePadding + col * (_itemSlotWidth + _itemSlotPadding);
 
-                    if (row == 0)
-                    {
+                    if (row == 0) {
                         childPosition.MinY = _edgePadding;
-                    }
-                    else
-                    {
+                    } else {
                         childPosition.MinY = Height - _edgePadding - (row * _itemSlotHeight) - (row - 1) * _itemSlotPadding;
                     }
 
@@ -92,7 +132,7 @@ namespace Isola.ui
                 }
             }
 
-            // Add "activeHotbarSlot" kodomo
+            // Add "activeHotbarSlot" child
             RPC basePosition = new();
             basePosition.MinX = _edgePadding - 1f;
             basePosition.MinY = _edgePadding - 1f;
@@ -101,42 +141,32 @@ namespace Isola.ui
             Children.Add("ActiveHotbarSlot", new ActiveHotbarSlotUI(_itemSlotHeight + 2f, _itemSlotWidth + 2f, eAnchor.None, 1f, true, true, false, 0, 152, 18, 18, basePosition, 0, (_cols * _rows - 1), 0));
         }
 
-        public void SetTextureCoords(float x, float y, float textureWidth, float textureHeight)
-        {
+        public void SetTextureCoords(float x, float y, float textureWidth, float textureHeight) {
             TexCoords = AtlasManager.GetPrecisionAtlasCoords(x, y, textureWidth, textureHeight);
         }
 
-        public override void OnMouseWheel(MouseWheelEventArgs e)
-        {
+        public override void OnMouseWheel(MouseWheelEventArgs e) {
             // Only run if ActiveHotbarSlotUI is accepting scrollwheel updates?
-            if (ToggleScrolling)
-            {
+            if (AllowScrolling) {
                 Children["ActiveHotbarSlot"].OnMouseWheel(e);
                 Children["ActiveHotbarSlot"].SetNDCs(Width, Height, NDCs);
             }
         }
 
-        public override void OnKeyDown(KeyboardKeyEventArgs e)
-        {
+        public override void OnKeyDown(KeyboardKeyEventArgs e) {
             base.OnKeyDown(e);
 
-            if (ToggleScrolling)
-            {
+            if (AllowScrolling) {
                 // drop one item into world if item exists at slot
-                if (e.Key == Keys.Q)
-                {
+                if (e.Key == Keys.Q) {
                     int index = ((ActiveHotbarSlotUI)Children["ActiveHotbarSlot"]).ActiveIndex;
 
                     ItemStack itemStack = Inventory.ItemStackList[index];
 
-                    if (!itemStack.Equals(default(ItemStack)))
-                    {
-                        if (itemStack.Amount == 1)
-                        {
+                    if (!itemStack.Equals(default(ItemStack))) {
+                        if (itemStack.Amount == 1) {
                             Inventory.ItemStackList[index] = default(ItemStack);
-                        }
-                        else
-                        {
+                        } else {
                             Inventory.ItemStackList[index].Amount -= 1;
                         }
 
@@ -146,27 +176,21 @@ namespace Isola.ui
             }
         }
 
-        public override void OnRightClick(Vector2 mouseNDCs, WorldManager world)
-        {
+        public override void OnRightClick(Vector2 mouseNDCs, WorldManager world) {
             int index = ((ActiveHotbarSlotUI)Children["ActiveHotbarSlot"]).ActiveIndex;
             
-            if (!Inventory.ItemStackList[index].Equals(default(ItemStack)))
-            {
-                if (AssetLibrary.GetItemFromItemName(Inventory.ItemStackList[index].ItemName, out var item))
-                {
+            if (!Inventory.ItemStackList[index].Equals(default(ItemStack))) {
+                if (AssetLibrary.GetItemFromItemName(Inventory.ItemStackList[index].ItemName, out var item)) {
                     item!.OnRightClick(mouseNDCs, world);
                 }
             }
         }
 
-        public override void OnLeftClick(Vector2 mouseNDCs, WorldManager world)
-        {
+        public override void OnLeftClick(Vector2 mouseNDCs, WorldManager world) {
             int index = ((ActiveHotbarSlotUI)Children["ActiveHotbarSlot"]).ActiveIndex;
 
-            if (!Inventory.ItemStackList[index].Equals(default(ItemStack)))
-            {
-                if (AssetLibrary.GetItemFromItemName(Inventory.ItemStackList[index].ItemName, out var item))
-                {
+            if (!Inventory.ItemStackList[index].Equals(default(ItemStack))) {
+                if (AssetLibrary.GetItemFromItemName(Inventory.ItemStackList[index].ItemName, out var item)) {
                     item!.OnLeftClick(mouseNDCs, world);
                 }
             }
