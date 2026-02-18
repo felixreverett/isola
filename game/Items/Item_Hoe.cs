f1d0320028dd9f82c291049ef274f5924929aec6
Felix Everett <felixreverett@gmail.com>
2026-02-18 17:19:51 +0000

    🧹 Refactored to access AssetLibrary through dependency injection; 🧹 Refactored file to use K&R styling.

--- game/Items/Item_Hoe.cs
+++ game/Items/Item_Hoe.cs
@@ -1,24 +1,23 @@
 ﻿using Isola.World;
+using Isola.Utilities;
 using OpenTK.Mathematics;
 
-namespace Isola.Items
-{
-    public class Item_Hoe : Item
-    {
+namespace Isola.Items {
+    public class Item_Hoe : Item {
         public Item_Hoe(string itemName, int textureIndex, int stackLimit = 1)
-            : base(itemName, textureIndex, stackLimit)
-        {
+            : base(itemName, textureIndex, stackLimit) {
         }
 
-        public override void OnRightClick(Vector2 mousePosition, WorldManager world)
+        public override void OnRightClick(Vector2 mousePosition, WorldManager world, AssetLibrary assets)
         {
-            if (world.GetTile(mousePosition.X, mousePosition.Y).GetTileName() == "Grass")
-            {
+            ChunkTile currentTile = world.GetTile(mousePosition.X, mousePosition.Y);
+
+            if (currentTile.GetTileName(assets) == "Grass") {
                 world.SetTile(mousePosition.X, mousePosition.Y, "Tilled Soil");
             }
         }
 
-        public override void OnLeftClick(Vector2 mousePosition, WorldManager world)
+        public override void OnLeftClick(Vector2 mousePosition, WorldManager world, AssetLibrary assets)
         {
 
         }
