ad4dcb3c9996abe47e3da2afc55747776ad73c81
Felix Everett <felixreverett@gmail.com>
2026-02-18 17:21:40 +0000

    🧹 Added CalculateTexCoords() to decouple need for reference to atlasmanager for each Tile

--- engine/utils/AssetLibrary.cs
+++ engine/utils/AssetLibrary.cs
@@ -3,47 +3,55 @@ using Isola.Drawing;
 using Isola.engine.graphics;
 using Isola.engine.graphics.text;
 using Isola.Items;
+using Microsoft.Extensions.Logging;
 
-namespace Isola.Utilities
-{
-    public static class AssetLibrary
-    {
-        public static List<Item>? ItemList;
-        public static List<TileData>? TileList;
-        public static Dictionary<string, IAtlasManager> TextureAtlasManagerList = new();
-        public static Dictionary<string, BatchRenderer> BatchRendererList = new();
-        public static Dictionary<string, Shader> ShaderList = new();
+namespace Isola.Utilities{
+    public class AssetLibrary {
+        public List<Item>? ItemList;
+        public List<TileData>? TileList;
+        public Dictionary<int, TileData> TileLookup { get; private set; } = new();
+        public Dictionary<string, IAtlasManager> TextureAtlasManagerList = new();
+        public Dictionary<string, BatchRenderer> BatchRendererList = new();
+        public Dictionary<string, Shader> ShaderList = new();
+        private readonly ILogger<AssetLibrary> _logger;
+        private readonly ILoggerFactory _loggerFactory;
 
-        public static bool GetItemFromItemName(string itemName, out Item? item)
-        {
+
+        public AssetLibrary(ILoggerFactory loggerFactory) {
+            _logger = loggerFactory.CreateLogger<AssetLibrary>();
+            _loggerFactory = loggerFactory;
+        }
+
+        // Single initialise to ensure assets are loaded in the correct order
+        public void Initialise() {
+            _logger.LogInformation("Initialising Asset Library...");
+            InitialiseShaders();
+            InitialiseTextureAtlasManagerList();
+            InitialiseItemList();
+            InitialiseTileList();
+        }
+
+        public bool GetItemFromItemName(string itemName, out Item? item) {
             item = ItemList!.FirstOrDefault(i => i.ItemName == itemName);
 
-            if (item != null)
-            {
+            if (item != null) {
                 return true;
-            }
-
-            else
-            {
+            } else {
                 return false;
             }
         }
 
-        public static bool GetTileIDFromTileName(string tileName, out int tileID)
-        {
+        public bool GetTileIDFromTileName(string tileName, out int tileID) {
             tileID = TileList.FirstOrDefault(i => i.Name == tileName).TileID;
             return tileID != -1;
         }
 
-        public static void InitialiseItemList()
-        {
-            if (ItemList is not null)
-            {
+        public void InitialiseItemList() {
+            if (ItemList is not null) {
                 return;
             }
 
-            ItemList = new List<Item>
-            {
+            ItemList = new List<Item> {
                 new Item("Persimmon", 1),
                 // Seeds
                 new Item_Seeds_WheatSeeds("Wheat Seeds", 43),
@@ -54,76 +62,56 @@ namespace Isola.Utilities
             };
         }
 
-        // Single intialise to ensure the correct order
-        public static void Initialise()
-        {
-            InitialiseShaders();
-            InitialiseTextureAtlasManagerList();
-            InitialiseItemList();
-            InitialiseTileList();
-        }
-
-        public static void InitialiseShaders()
-        {
+        public void InitialiseShaders() {
             Shader WorldShader = new(Shader.ParseShader(@"../../../resources/shaders/TextureWithColorAndTextureSlotAndUniforms.glsl"));
-            if (WorldShader.CompileShader())
-            {
+            if (WorldShader.CompileShader()) {
                 WorldShader.SetTextureArray();
                 ShaderList.Add("World Shader", WorldShader);
-            }
-            else
-            {
-                Console.WriteLine("Failed to compile world shader.");
+            } else {
+                _logger.LogError("Failed to compile world shader.");
             }
 
             Shader UIShader = new(Shader.ParseShader(@"../../../resources/shaders/UIShader.glsl")); // todo: move to UI Load?
-            if (UIShader.CompileShader())
-            {
+            if (UIShader.CompileShader()) {
                 UIShader.SetTextureArray();
                 ShaderList.Add("UI Shader", UIShader);
-            }
-            else { Console.WriteLine("Failed to compile UI shader.");
+            } else {
+                _logger.LogError("Failed to compile UI shader.");
             }
 
             Shader ScreenQuadShader = new(Shader.ParseShader(@"../../../resources/shaders/ScreenQuadShader.glsl"));
-            if (ScreenQuadShader.CompileShader())
-            {
+            if (ScreenQuadShader.CompileShader()) {
                 ShaderList.Add("Screen Quad Shader", ScreenQuadShader);
-            }
-            else
-            {
-                Console.WriteLine("Failed to compile Screen Quad Shader.");
+            } else {
+                _logger.LogError("Failed to compile Screen Quad shader.");
             }
         }
 
-        public static void InitialiseTextureAtlasManagerList()
-        {
+        public void InitialiseTextureAtlasManagerList() {
             TextureAtlasManagerList.Add("Tile Atlas", new IndexedTextureAtlasManager("atlases/1024 Tile Atlas x16.png", 1024, 16, 8, true, 0.0f));
-            //TextureAtlasManagerList.Add("Player Atlas", new PrecisionTextureAtlasManager("atlases/Player.png", 1024, 1024, 1024, false, 0.001f));//todo Aug-2025: deprecate
             TextureAtlasManagerList.Add("Inventory Atlas", new PrecisionTextureAtlasManager("atlases/1024 UI Atlas x16.png", 1024, 1024, 1024, false));
             TextureAtlasManagerList.Add("Item Atlas", new IndexedTextureAtlasManager("atlases/1024 Item Atlas 16x.png", 1024, 16, 8, false, 0.001f));
             TextureAtlasManagerList.Add("Font Atlas", new FontAtlasManager("nosutaru", 4));
             TextureAtlasManagerList.Add("Entity Atlas", new EntityTextureAtlasManager("atlases/1024 Entity Atlas x16.png", 1024, 1024));
-            // Todo: add entity atlas here
 
-            // texture unit 0 is dedicated to the frame buffer object and mustn't be used
-            BatchRendererList.Add("Tile Atlas", new BatchRenderer(ShaderList["World Shader"], 1, "atlases/1024 Tile Atlas x16.png", 0.0f));
-            //BatchRendererList.Add("Player Atlas", new BatchRenderer(1, "atlases/Player.png")); //todo Aug-2025: deprecate
-            BatchRendererList.Add("Inventory Atlas", new BatchRenderer(ShaderList["UI Shader"], 2, "atlases/1024 UI Atlas x16.png"));
-            BatchRendererList.Add("Item Atlas", new BatchRenderer(ShaderList["UI Shader"], 3, "atlases/1024 Item Atlas 16x.png"));
-            BatchRendererList.Add("Font Atlas", new BatchRenderer(ShaderList["UI Shader"], 4, "fonts/nosutaru.png"));
-            BatchRendererList.Add("Entity Atlas", new BatchRenderer(ShaderList["World Shader"], 5, "atlases/1024 Entity Atlas 16x.png", 0.001f));
+            ILogger<BatchRenderer> batchLogger = _loggerFactory.CreateLogger<BatchRenderer>();
+
+            // [!] texture unit 0 is dedicated to the frame buffer object and mustn't be used
+            BatchRendererList.Add("Tile Atlas", new BatchRenderer(ShaderList["World Shader"], 1, "atlases/1024 Tile Atlas x16.png", batchLogger, 0.0f));
+            BatchRendererList.Add("Inventory Atlas", new BatchRenderer(ShaderList["UI Shader"], 2, "atlases/1024 UI Atlas x16.png", batchLogger));
+            BatchRendererList.Add("Item Atlas", new BatchRenderer(ShaderList["UI Shader"], 3, "atlases/1024 Item Atlas 16x.png", batchLogger));
+            BatchRendererList.Add("Font Atlas", new BatchRenderer(ShaderList["UI Shader"], 4, "fonts/nosutaru.png", batchLogger));
+            BatchRendererList.Add("Entity Atlas", new BatchRenderer(ShaderList["World Shader"], 5, "atlases/1024 Entity Atlas 16x.png", batchLogger, 0.001f));
+
+            _logger.LogInformation("All BatchRenderers initialised.");
         }
 
-        public static void InitialiseTileList()
-        {
-            if (TileList is not null)
-            {
+        public void InitialiseTileList() {
+            if (TileList is not null) {
                 return;
             }
 
-            TileList = new List<TileData>
-            {
+            TileList = new List<TileData> {
                 new TileData("Grass", 0, 1, false),
                 new TileData("Sand", 1, 2, false),
                 new TileData("Water", 2, 3, true),
@@ -132,6 +120,16 @@ namespace Isola.Utilities
                 new TileData("Water_4", 5, 6, true),
                 new TileData("Tilled Soil", 7, 7, false)
             };
+
+            TileLookup = TileList.ToDictionary(t => t.TileID);
+
+            var atlas = (IndexedTextureAtlasManager)TextureAtlasManagerList["Tile Atlas"];
+
+            foreach (var tile in TileList) {
+                tile.CalculateTexCoords(atlas);
+            }
+
+            _logger.LogInformation("Tile coordinates calculated for {Count} tiles.", TileList.Count);
         }
     }
 }
--- game/World/TileData.cs
+++ game/World/TileData.cs
@@ -6,16 +6,12 @@ namespace Isola
     /// <summary>
     /// A class used to store static tile information that doesn't need to be repeated in world data
     /// </summary>
-    public class TileData
-    {
-        public string Name { get; private set; }
-        public int TileID { get; private set; }
-        public int TextureIndex { get; private set; }
-        public bool IsCollidable { get; private set; } = false;
-        
-        protected IndexedTextureAtlasManager AtlasManager = (IndexedTextureAtlasManager)AssetLibrary.TextureAtlasManagerList["Tile Atlas"];
+    public class TileData {
+        public string Name { get; init; }
+        public int TileID { get; init; }
+        public int TextureIndex { get; init; }
+        public bool IsCollidable { get; init; }      
         public TexCoords TexCoords;
-
 
         public TileData(string name, int tileID, int textureIndex, bool isCollidable)
         {
@@ -23,7 +19,10 @@ namespace Isola
             TileID = tileID;
             TextureIndex = textureIndex;
             IsCollidable = isCollidable;
-            TexCoords = AtlasManager.GetIndexedAtlasCoords(textureIndex);
+        }
+
+        public void CalculateTexCoords(IndexedTextureAtlasManager atlasManager) {
+            TexCoords = atlasManager.GetIndexedAtlasCoords(TextureIndex);
         }
 
     }
