161e6fb543a15c584ed4d7b5d1cac56e64651960
Felix Everett <felixreverett@gmail.com>
2026-02-18 17:16:49 +0000

    🧹 Refactored to access AssetLibrary through dependency injection; 🧹 Refactored file to use K&R styling.

--- game/Entities/ItemEntity.cs
+++ game/Entities/ItemEntity.cs
@@ -1,49 +1,42 @@
-﻿using Isola.Drawing;
-using Isola.engine.graphics;
+﻿using Isola.engine.graphics;
 using Isola.Utilities;
 using OpenTK.Mathematics;
 using System.Text.Json;
 
-namespace Isola.Entities
-{
-    public class ItemEntity : Entity
-    {
+namespace Isola.Entities {
+    public class ItemEntity : Entity {
         public string ItemName { get; set; }
         public int Amount { get; set; }
         private EntityTextureAtlasManager AtlasManager { get; set; }
 
         // Initialize entity otherwise
-        public ItemEntity(Vector2 position, string itemName, int amount) 
-            : base(position)
-        {
+        public ItemEntity(Vector2 position, string itemName, int amount, AssetLibrary assets) 
+            : base(position, assets) {
             this.ItemName = itemName;
             this.Amount = amount;
-            AtlasManager = (EntityTextureAtlasManager)AssetLibrary.TextureAtlasManagerList["Entity Atlas"];
-            BatchRenderer = AssetLibrary.BatchRendererList["Entity Atlas"];
+            AtlasManager = (EntityTextureAtlasManager)_assets.TextureAtlasManagerList["Entity Atlas"];
+            BatchRenderer = _assets.BatchRendererList["Entity Atlas"];
             TexCoords = AtlasManager.GetAtlasCoords(ItemName);
         }
 
         // Initialize Entity from save data
-        public ItemEntity(ItemEntitySaveData saveData)
-            : base(saveData)
-        {
+        public ItemEntity(ItemEntitySaveData saveData, AssetLibrary assets)
+            : base(saveData, assets) {
             this.ItemName = saveData.ItemName;
             this.Amount = saveData.Amount;
-            AtlasManager = (EntityTextureAtlasManager)AssetLibrary.TextureAtlasManagerList["Entity Atlas"];
-            BatchRenderer = AssetLibrary.BatchRendererList["Entity Atlas"];
+            AtlasManager = (EntityTextureAtlasManager)_assets.TextureAtlasManagerList["Entity Atlas"];
+            BatchRenderer = _assets.BatchRendererList["Entity Atlas"];
             TexCoords = AtlasManager.GetAtlasCoords(ItemName);
         }
 
         // Export entity save data
-        public override EntitySaveDataObject GetSaveData()
-        {
-            ItemEntitySaveData data = new
-                (
-                    new float[] { Position.X, Position.Y },     // 0
-                    new float[] { Size.X, Size.Y },             // 1
-                    ItemName,                                   // 2
-                    Amount                                      // 3
-                );
+        public override EntitySaveDataObject GetSaveData() {
+            ItemEntitySaveData data = new (
+                new float[] { Position.X, Position.Y },     // 0
+                new float[] { Size.X, Size.Y },             // 1
+                ItemName,                                   // 2
+                Amount                                      // 3
+            );
 
             return new EntitySaveDataObject(eEntityType.ItemEntity, JsonSerializer.Serialize(data));
         }
