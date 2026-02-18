ca7f0796615bbb70ac0e19e1d1ab88678271a645
Felix Everett <felixreverett@gmail.com>
2026-02-18 17:20:07 +0000

    🧹 Refactored to access AssetLibrary through dependency injection; 🧹 Refactored file to use K&R styling.

--- game/Items/Item_WoodChest.cs
+++ game/Items/Item_WoodChest.cs
@@ -1,24 +1,20 @@
 ﻿using Isola.Entities;
+using Isola.Utilities;
 using Isola.World;
 using OpenTK.Mathematics;
 
-namespace Isola.Items
-{
-    public class Item_WoodChest : Item
-    {
+namespace Isola.Items {
+    public class Item_WoodChest : Item {
         public Item_WoodChest(string itemName, int textureIndex, int stackLimit = 999)
-            : base(itemName, textureIndex, stackLimit)
-        {
+            : base(itemName, textureIndex, stackLimit) {
         }
 
-        public override void OnRightClick(Vector2 mousePosition, WorldManager world)
-        {
+        public override void OnRightClick(Vector2 mousePosition, WorldManager world, AssetLibrary assets) {
             Console.WriteLine($"Adding new {ItemName} to world.");
-            world.AddEntityToWorld(new TileEntity_Chest(mousePosition, new Vector2(0f, 0f)));
+            world.AddEntityToWorld(new TileEntity_Chest(mousePosition, new Vector2(0f, 0f), assets));
         }
 
-        public override void OnLeftClick(Vector2 mousePosition, WorldManager world)
-        {
+        public override void OnLeftClick(Vector2 mousePosition, WorldManager world, AssetLibrary assets) {
 
         }
     }
