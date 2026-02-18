5ffb2665e45f1f0008a316adef980ed37c88ad38
Felix Everett <felixreverett@gmail.com>
2026-02-18 17:18:18 +0000

    🧹 Refactored to access AssetLibrary through dependency injection; 🧹 Refactored file to use K&R styling.

--- game/Entities/TileEntity.cs
+++ game/Entities/TileEntity.cs
@@ -1,40 +1,34 @@
-﻿using Isola.Drawing;
-using Isola.engine.graphics;
+﻿using Isola.engine.graphics;
 using Isola.Entities;
 using Isola.Utilities;
 using OpenTK.Mathematics;
 using System.Text.Json;
 
-namespace Isola
-{
-    public abstract class TileEntity : Entity
-    {
+namespace Isola {
+    public abstract class TileEntity : Entity {
         internal Vector2 DrawPositionOffset { get; set; } = new Vector2(0f, 0f);
         private EntityTextureAtlasManager AtlasManager { get; set; }
 
         // Initialize TileEntity from save data
-        public TileEntity(TileEntitySaveData saveData)
-            : base(saveData)
-        {
+        public TileEntity(TileEntitySaveData saveData, AssetLibrary assets)
+            : base(saveData, assets) {
             DrawPositionOffset = new Vector2(saveData.DrawPositionOffset[0], saveData.DrawPositionOffset[1]);
             AlignPosition();
             Size = new Vector2(saveData.Size[0], saveData.Size[1]);
-            AtlasManager = (EntityTextureAtlasManager)AssetLibrary.TextureAtlasManagerList["Entity Atlas"];
-            BatchRenderer = AssetLibrary.BatchRendererList["Entity Atlas"];
+            AtlasManager = (EntityTextureAtlasManager)_assets.TextureAtlasManagerList["Entity Atlas"];
+            BatchRenderer = _assets.BatchRendererList["Entity Atlas"];
         }
 
         // Default constructor
-        public TileEntity(Vector2 position, Vector2 drawPositionOffset)
-            : base(position)
-        {
+        public TileEntity(Vector2 position, Vector2 drawPositionOffset, AssetLibrary assets)
+            : base(position, assets) {
             AlignPosition();
-            AtlasManager = (EntityTextureAtlasManager)AssetLibrary.TextureAtlasManagerList["Entity Atlas"];
-            BatchRenderer = AssetLibrary.BatchRendererList["Entity Atlas"];
+            AtlasManager = (EntityTextureAtlasManager)_assets.TextureAtlasManagerList["Entity Atlas"];
+            BatchRenderer = _assets.BatchRendererList["Entity Atlas"];
         }
 
         // TileEntities can have an offset for drawing
-        public override void Draw()
-        {
+        public override void Draw() {
             Box2 rect = new Box2(
                 Position.X - Size.X / 2f + DrawPositionOffset.X, Position.Y + DrawPositionOffset.Y,
                 Position.X + Size.X / 2f + DrawPositionOffset.X, Position.Y + Size.Y + DrawPositionOffset.Y
@@ -44,20 +38,17 @@ namespace Isola
         }
 
         // Export entity save data
-        public override EntitySaveDataObject GetSaveData()
-        {
-            TileEntitySaveData data = new
-                (
-                    new float[] { Position.X, Position.Y },                     // 0
-                    new float[] { Size.X, Size.Y },                             // 1
-                    new float[] { DrawPositionOffset.X, DrawPositionOffset.Y }  // 2
-                );
+        public override EntitySaveDataObject GetSaveData() {
+            TileEntitySaveData data = new (
+                new float[] { Position.X, Position.Y },                     // 0
+                new float[] { Size.X, Size.Y },                             // 1
+                new float[] { DrawPositionOffset.X, DrawPositionOffset.Y }  // 2
+            );
 
             return new EntitySaveDataObject(eEntityType.TileEntity, JsonSerializer.Serialize(data));
         }
 
-        private void AlignPosition()
-        {
+        private void AlignPosition() {
             Position = new Vector2((float)Math.Floor(Position.X) + 0.5f, (float)Math.Floor(Position.Y));
         }
     }
