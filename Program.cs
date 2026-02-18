6e60eaf74918f69053c73bc0e1f0e98e5f97f658
Felix Everett <felixreverett@gmail.com>
2026-02-18 17:24:15 +0000

    ✨ Introduced Logger system to propagate through relevant parts of the program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Isola
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Game1 game = new Game1(1920, 1080, "Isola alpha v0.1.3");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Isola {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // Config for logging system
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => {
+                builder.AddConfiguration(config.GetSection("Logging"))
+                       .AddConsole();
+            });
+
+            ILogger<Game1> logger = loggerFactory.CreateLogger<Game1>();
+
+            Game1 game = new Game1(1920, 1080, "Isola alpha v0.1.3", loggerFactory);
             game.Run();
         }
     }
